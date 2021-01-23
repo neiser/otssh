fdb201e9fd1e5dc88af774e2111ae50b687a46b9
Andreas Grub <andreas.grub@qaware.de>
2021-01-23 18:00:13 +0100

    Strip down example from docs to Pty/Shell

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ extern crate thrussh;
 extern crate thrussh_keys;
 extern crate tokio;
 
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use thrussh::*;
 use thrussh::server::{Auth, Session};
@@ -12,38 +11,24 @@ use thrussh_keys::*;
 
 #[tokio::main]
 async fn main() {
-    let client_key = thrussh_keys::key::KeyPair::generate_ed25519().unwrap();
-    let client_pubkey = Arc::new(client_key.clone_public_key());
     let mut config = thrussh::server::Config::default();
-    config.connection_timeout = Some(std::time::Duration::from_secs(3));
-    config.auth_rejection_time = std::time::Duration::from_secs(3);
-    config.keys.push(thrussh_keys::key::KeyPair::generate_ed25519().unwrap());
+    config.connection_timeout = Some(std::time::Duration::from_secs(10));
+    config.auth_rejection_time = std::time::Duration::from_secs(1);
+    let server_key = thrussh_keys::key::KeyPair::generate_ed25519().unwrap();
+    config.keys.push(server_key);
     let config = Arc::new(config);
-    let sh = Server {
-        client_pubkey,
-        clients: Arc::new(Mutex::new(HashMap::new())),
-        id: 0,
-    };
-    tokio::time::timeout(
-        std::time::Duration::from_secs(1),
-        thrussh::server::run(config, "0.0.0.0:2222", sh),
-    ).await.unwrap_or(Ok(()));
+    let sh = Server {};
+    thrussh::server::run(config, "0.0.0.0:2222", sh).await;
 }
 
 #[derive(Clone)]
-struct Server {
-    client_pubkey: Arc<thrussh_keys::key::PublicKey>,
-    clients: Arc<Mutex<HashMap<(usize, ChannelId), thrussh::server::Handle>>>,
-    id: usize,
-}
+struct Server {}
 
 impl server::Server for Server {
     type Handler = Self;
 
     fn new(&mut self, _: Option<std::net::SocketAddr>) -> Self {
-        let s = self.clone();
-        self.id += 1;
-        s
+        self.clone()
     }
 }
 
@@ -54,39 +39,42 @@ impl server::Handler for Server {
     type FutureBool = futures::future::Ready<Result<(Self, Session, bool), anyhow::Error>>;
 
     fn finished_auth(self, auth: Auth) -> Self::FutureAuth {
+        println!("finished_auth");
         futures::future::ready(Ok((self, auth)))
     }
 
     fn finished_bool(self, b: bool, s: Session) -> Self::FutureBool {
+        println!("finished_bool");
         futures::future::ready(Ok((self, s, b)))
     }
 
     fn finished(self, s: Session) -> Self::FutureUnit {
+        println!("finished");
         futures::future::ready(Ok((self, s)))
     }
 
     fn auth_publickey(self, _: &str, _: &key::PublicKey) -> Self::FutureAuth {
+        println!("auth_publickey");
         self.finished_auth(server::Auth::Accept)
     }
 
     fn channel_open_session(self, channel: ChannelId, session: Session) -> Self::FutureUnit {
-        {
-            let mut clients = self.clients.lock().unwrap();
-            clients.insert((self.id, channel), session.handle());
-        }
+        println!("channel_open_session: {:?}", channel);
         self.finished(session)
     }
 
-    fn data(self, channel: ChannelId, data: &[u8], mut session: Session) -> Self::FutureUnit {
-        {
-            let mut clients = self.clients.lock().unwrap();
-            for ((id, channel), ref mut s) in clients.iter_mut() {
-                if *id != self.id {
-                    s.data(*channel, CryptoVec::from_slice(data));
-                }
-            }
-        }
-        session.data(channel, CryptoVec::from_slice(data));
+    fn data(self, channel: ChannelId, data: &[u8], session: Session) -> Self::FutureUnit {
+        println!("Channel {:?}: {:?}", channel, data);
+        self.finished(session)
+    }
+
+    fn pty_request(self, channel: ChannelId, term: &str, col_width: u32, row_height: u32, pix_width: u32, pix_height: u32, modes: &[(Pty, u32)], session: Session) -> Self::FutureUnit {
+        println!("pty_request {}, {:?}", term, modes);
+        self.finished(session)
+    }
+
+    fn shell_request(self, channel: ChannelId, session: Session) -> Self::FutureUnit {
+        println!("shell_request");
         self.finished(session)
     }
 }
